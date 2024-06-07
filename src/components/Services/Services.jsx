318615daf971e3e5064b118bf4e2712b4e7bbf84
unknown <kariukibenard189@gmail.com>
2024-06-07 12:13:15 +0300

    Changed the what I do cards content.

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -8,8 +8,8 @@ export default function Services() {
       icon: <i class="fa-solid fa-code"></i>,
       title: "Back-end Web Developer",
       writeUp: `I specialize in building robust and scalable
-       software solutions tailored to meet diverse user
-        needs and business requirements.`,
+       back ends tailored to meet diverse user
+        needs and business requirements using NodeJS and Express.`,
     },
     {
       index: "02",
@@ -17,14 +17,15 @@ export default function Services() {
       title: "Mobile Application Developer",
       writeUp: `With a keen eye for detail and a focus
        on user-centric design principles, I create
-        visually stunning interfaces that enhance user
+        fully functioning mobile apps that enhance user
          experience and engagement.`,
     },
   ];
   return (
     <section
       id="Services"
-      className="py-10 rounded-lg lg:rounded-none bg-[#131313]">
+      className="py-10 rounded-lg lg:rounded-none bg-[#131313]"
+    >
       <div>
         <p className="text-center opacity-[.5] text-sm">SERVICES</p>
         <h1 className="text-center text-2xl">
@@ -42,16 +43,17 @@ export default function Services() {
               initial="hidden"
               whileInView="show"
               viewport={{ once: true }}
-
               key={card.index}
-              className={`${cardStyle} relative sm:w-[25rem] border-2 border-solid border-myColor rounded-xl py-10 px-4`}>
+              className={`${cardStyle} relative sm:w-[25rem] border-2 border-solid border-myColor rounded-xl py-10 px-4`}
+            >
               <h1 className="absolute right-[2rem] top-[1rem] text-4xl font-medium opacity-[.3]">
                 {card.index}
               </h1>
               <div className="text-2xl mb-4">{card.icon}</div>
               <h1 className="text-xl mb-4 font-medium">{card.title}</h1>
               <div
-                className={`${lineStyle} w-[5rem] h-[2px] rounded-full mb-2 opacity `}></div>
+                className={`${lineStyle} w-[5rem] h-[2px] rounded-full mb-2 opacity `}
+              ></div>
               <p>{card.writeUp}</p>
             </motion.div>
           );
