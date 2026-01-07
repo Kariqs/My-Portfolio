77327c6fb73ed0f6b45f416673644135a8aa6bf4
Kariqs <kariukibenard189@gmail.com>
2026-01-07 21:30:20 +0300

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,60 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface NavItem {
+  id: string;
+  title: string;
+  description?: string;
+  isExternal?: boolean;
+  url?: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('portfolio');
+  activeSection: string | null = null;
+
+  navItems: NavItem[] = [
+    {
+      id: 'about',
+      title: 'About Me',
+      description: `
+      Software Engineer specializing in Angular, TypeScript, Golang and modern web technologies.
+Passionate about clean code, performance and beautiful user experiences.
+Based in Kenya.
+      `,
+    },
+    {
+      id: 'works',
+      title: 'Works',
+      isExternal: true,
+      url: '/works',
+    },
+    {
+      id: 'resume',
+      title: 'Resume',
+      isExternal: true,
+      url: '/assets/documents/Resume.pdf',
+    },
+    {
+      id: 'skills',
+      title: 'Skills',
+      description:
+        'Proficient in full-stack development with expertise in front-end technologies like Angular, React, and Vue.js, as well as back-end frameworks including Node.js, .NET, and Django. Experienced in database management, cloud services, and modern DevOps practices.',
+    },
+    {
+      id: 'contacts',
+      title: 'Contacts',
+      description:
+        "Get in touch for collaborations, commissions, or inquiries. I'm always excited to connect with fellow creatives and art enthusiasts.",
+    },
+  ];
+
+  toggleSection(section: string) {
+    this.activeSection = this.activeSection === section ? null : section;
+  }
 }
